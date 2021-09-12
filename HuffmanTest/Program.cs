00860dc73b087a7a7fd205485851a50421ab9158
Ghandalf312 <djmoralesm@correo.url.edu.gt>
2021-09-12 17:56:22 -0600

    Proyecto de consola creado, empezando con la compresion

---
+++ HuffmanTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HuffmanTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
