b04c6f40afe337be03bc2dfff5b2289b830b1d7a
Ghandalf312 <djmoralesm@correo.url.edu.gt>
2021-09-12 00:34:24 -0600

    Libreria e interfaz del compresor creada

---
+++ ClassLibrary/Structures/ICompressor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+namespace CustomGenerics.Structures
+{
+    interface ICompressor
+    {
+        Task CompressFile(string path, IFormFile file, string name);
+        Task DecompressFile(IFormFile file, string name);
+        string CompressText(string text);
+        string DecompressText(string text);
+    }
+}
---
+++ ClassLibrary/Structures/IProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Structures
+{
+    public interface IProbability
+    {
+        double GetProbability();
+        int GetFrequency();
+        byte GetValue();
+        void AddFrecuency(int number);
+        void SetByte(byte value);
+        void CalculateProbability(double totalBytes);
+        void SetProbability(double number);
+    }
+}
