21aa3676a439179733e1387e2048f29102c1dfc0
ZeroJChang <jdeleonchang@gmail.com>
2021-09-12 19:04:09 -0600

    Se arreglaron cambios

--- ClassLibrary/Utilities/ByteGenerator.cs
+++ ClassLibrary/Utilities/ByteGenerator.cs
@@ -6,5 +6,23 @@ namespace ClassLibrary.Utilities
 {
     class ByteGenerator
     {
+        static Encoding e = Encoding.GetEncoding("iso-8859-1");
+        public static byte[] ConvertToBytes(string text)
+        {
+            return e.GetBytes(text);
+        }
+
+        public static byte ConvertToByte(char character)
+        {
+            char[] convert = new char[1];
+            convert[0] = character;
+            var bytes = e.GetBytes(convert);
+            return bytes[0];
+        }
+
+        public static string ConvertToString(byte[] bytes)
+        {
+            return e.GetString(bytes);
+        }
     }
 }
