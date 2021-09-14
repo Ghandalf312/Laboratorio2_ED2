71c77a49f751e5086afeb4614b5a1f88a954b336
Ghandalf312 <djmoralesm@correo.url.edu.gt>
2021-09-14 00:37:00 -0600

    Empezando serie III api

---
+++ api/Models/HuffmanCompressions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+
+namespace api.Models
+{
+    public class HuffmanCompressions
+    {
+        public string OriginalName { get; set; }
+        public string CompressedName { get; set; }
+        public string CompressedFilePath { get; set; }
+        public double CompressionRatio { get; set; }
+        public double CompressionFactor { get; set; }
+        public double ReductionPercentage { get; set; }
+    }
+}
---
+++ api/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using ClassLibrary.Structures;
+
+namespace api
+{
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Singleton();
+                return _instance;
+            }
+        }
+        public Huffman<HuffmanCharacter> HuffmanTree;
+        public List<HuffmanCompressions> HistoryList = new List<HuffmanCompressions>();
+    }
+}
